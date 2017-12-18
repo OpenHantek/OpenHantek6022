89e9b85a72b8b22911186a8b0837d50087778b51
David Graeff <david.graeff@web.de>
2017-12-18 13:28:23 +0100

    Add forgotten files

---
+++ openhantek/src/viewconstants.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define DIVS_TIME 10.0   ///< Number of horizontal screen divs
+#define DIVS_VOLTAGE 8.0 ///< Number of vertical screen divs
+#define DIVS_SUB 5       ///< Number of sub-divisions per div
+
