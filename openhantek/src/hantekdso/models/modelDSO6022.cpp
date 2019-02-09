6026be6d4e84f459069dcf68161f00458ce0f51a
Ho-Ro <Ho-Ro@users.noreply.github.com>
2019-02-09 11:30:14 +0100

    calibration values for my scope, changed voltage display from p-p to avg, set GLES as default
    Signed-off-by: Ho-Ro <Ho-Ro@users.noreply.github.com>

--- openhantek/src/hantekdso/models/modelDSO6022.cpp
+++ openhantek/src/hantekdso/models/modelDSO6022.cpp
@@ -27,11 +27,13 @@ static void initSpecifications(Dso::ControlSpecification& specification) {
     specification.samplerate.multi.recordLengths = {UINT_MAX, 20480};
     specification.bufferDividers = { 1000 , 1 , 1 };
     // This data was based on testing and depends on Divider.
-    specification.voltageLimit[0] = { 25 , 51 , 103 , 206 , 412 , 196 , 392 , 784 , 1000 };
-    specification.voltageLimit[1] = { 25 , 51 , 103 , 206 , 412 , 196 , 392 , 784 , 1000 };
+    // The sample value at the top of the screen
+    specification.voltageLimit[0] = { 20 , 40 , 101 , 202 , 404 , 208 , 410 , 410 , 1060 };
+    specification.voltageLimit[1] = { 20 , 40 , 100 , 200 , 400 , 208 , 410 , 410 , 1060 };
     // Divider. Tested and calculated results are different!
-    specification.gain = { {10,0.08} , {10,0.16} , {10,0.40} , {10,0.80} ,
-                           {10,1.60} , {2,4.00} , {2,8.00} , {2,16.00} , {1,40.00} };
+    // HW gain, voltage steps in V/screenheight (ranges 10,20,50,100,200,500,1000,2000,5000 mV)
+    specification.gain = { {10,0.08} , {10,0.16} , {10,0.40} , {10,0.80} , {10,1.60} ,
+                           {2,4.00} , {2,8.00} , {1,16.00} , {1,40.00} };
     specification.fixedSampleRates = { {10,1e5} , {20,2e5} , {50,5e5} , {1,1e6} , {2,2e6} , {4,4e6} , {8,8e6} ,
                                        {16,16e6} , {24,24e6} , {48,48e6} };
     specification.sampleSize = 8;
