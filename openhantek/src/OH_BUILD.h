2cb1ce0921561fb435079204eddf2604b7bf9a45
Martin <Ho-Ro@users.noreply.github.com>
2019-09-17 21:10:38 +0200

    typo
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- openhantek/src/OH_BUILD.h
+++ openhantek/src/OH_BUILD.h
@@ -1,3 +1,2 @@
--e // Do not edit, will be re-created at each commit!
-
-#define OH_BUILD "20190917 build 463"
+// Do not edit, will be re-created at each commit!
+#define OH_BUILD "20190917 build 464"
