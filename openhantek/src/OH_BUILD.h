OpenHantek/OpenHantek6022

commit 3f6b16c209332357de642a0b2f6e636056c46384

Add check if scope has CONTROL_SETCOUPLING controlCode before sending it

openhantek/src/docks/VoltageDock.cpp
@@ -6,6 +6,7 @@
 #include <QDockWidget>
 #include <QLabel>
 #include <QSignalBlocker>
+#include <QDebug>
 
 #include <cmath>
 
@@ -44,17 +45,17 @@ VoltageDock::VoltageDock(DsoSettingsScope *scope, const Dso::ControlSpecificatio
     dockLayout = new QGridLayout();
     dockLayout->setColumnMinimumWidth(0, 64);
     dockLayout->setColumnStretch(1, 1);
-
+    dockLayout->setSpacing(5);
     // Initialize elements
     int row = 0;
     for (ChannelID channel = 0; channel < scope->voltage.size(); ++channel) {
         ChannelBlock b;
 
-        b.miscComboBox=(new QComboBox());
-        b.gainComboBox=(new QComboBox());
-        b.invertCheckBox=(new QCheckBox(tr("Invert")));
-        b.attnCheckBox=(new QCheckBox(tr("X10")));
-        b.usedCheckBox=(new QCheckBox(scope->voltage[channel].name));
+        b.miscComboBox = new QComboBox();
+        b.gainComboBox = new QComboBox();
+        b.invertCheckBox = new QCheckBox(tr("Invert"));
+        b.attnCheckBox = new QCheckBox(tr("x10"));
+        b.usedCheckBox = new QCheckBox(scope->voltage[channel].name);
 
         channelBlocks.push_back(std::move(b));
 
@@ -93,12 +94,12 @@ VoltageDock::VoltageDock(DsoSettingsScope *scope, const Dso::ControlSpecificatio
 
         connect(b.gainComboBox, SELECT<int>::OVERLOAD_OF(&QComboBox::currentIndexChanged), [this,channel](int index) {
             this->scope->voltage[channel].gainStepIndex = (unsigned)index;
-            emit gainChanged(channel, this->scope->gain(channel));
+            emit gainChanged( channel, this->scope->gain(channel) );
         });
         connect(b.attnCheckBox, &QAbstractButton::toggled, [this,channel](bool attn) {
-            this->setAttn( channel, attn );
             this->scope->voltage[channel].probeUsed = attn;
             this->scope->voltage[channel].probeAttn = attn ? ATTENUATION : 1;
+            setAttn( channel, attn );
             emit probeAttnChanged( channel, attn, attn ? ATTENUATION : 1 ); // make sure to set the probe first, since this will influence the gain
             emit gainChanged(channel, this->scope->gain(channel));
         });
@@ -107,11 +108,11 @@ VoltageDock::VoltageDock(DsoSettingsScope *scope, const Dso::ControlSpecificatio
             emit invertedChanged( channel, checked );
         });
         connect(b.miscComboBox, SELECT<int>::OVERLOAD_OF(&QComboBox::currentIndexChanged), [this,channel,spec,scope](int index) {
+            this->scope->voltage[channel].couplingOrMathIndex = (unsigned)index;
             if (channel < spec->channels) {
-                this->setCoupling(channel, (unsigned)index);
+                //setCoupling(channel, (unsigned)index);
                 emit couplingChanged(channel, scope->coupling(channel, spec));
             } else {
-                this->scope->voltage[channel].couplingOrMathIndex = (unsigned)index;
                 emit modeChanged(Dso::getMathMode(this->scope->voltage[channel]));
             }
         });
@@ -136,8 +137,7 @@ void VoltageDock::setCoupling(ChannelID channel, unsigned couplingIndex) {
     if (channel >= spec->channels) return;
     if (couplingIndex >= spec->couplings.size()) return;
     QSignalBlocker blocker(channelBlocks[channel].miscComboBox);
-    channelBlocks[channel].miscComboBox->setCurrentIndex((int)couplingIndex);
-    this->scope->voltage[channel].couplingOrMathIndex = couplingIndex;
+    channelBlocks[channel].miscComboBox->setCurrentIndex((int)couplingIndex);    
 }
 
 void VoltageDock::setGain(ChannelID channel, unsigned gainStepIndex) {
@@ -155,8 +155,6 @@ void VoltageDock::setAttn(ChannelID channel, bool attn) {
     channelBlocks[channel].gainComboBox->addItems( attn ? attnStrings : gainStrings );
     channelBlocks[channel].gainComboBox->setCurrentIndex( index );
     channelBlocks[channel].attnCheckBox->setChecked(attn);
-    this->scope->voltage[channel].probeUsed = attn;
-    this->scope->voltage[channel].probeAttn = attn ? ATTENUATION : 1.0;
 }
 
 void VoltageDock::setMode(unsigned mathModeIndex) {

openhantek/CMakeLists.txt
@@ -34,6 +34,9 @@ set(EXECTYPE "")
 if(WIN32 AND MINGW)
     set(EXECTYPE WIN32)
 endif()
+if(APPLE)
+    set(EXECTYPE MACOSX_BUNDLE)
+endif()
 
 # make executable
 add_executable(${PROJECT_NAME} ${EXECTYPE} ${SRC} ${HEADERS} ${UI} ${QRC} ${TRANSLATION_BIN_FILES} ${TRANSLATION_QRC})
@@ -66,7 +69,10 @@ if(NOT WIN32)
 endif()
 
 # install commands
-install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "bin")
-
+if (APPLE)
+   install(TARGETS ${PROJECT_NAME} BUNDLE DESTINATION ${CMAKE_BINARY_DIR}/${PROJECT_NAME}.app/Contents/MacOS/${PROJECT_NAME})
+else()
+	install(TARGETS ${PROJECT_NAME} RUNTIME DESTINATION "bin")
+endif()
 include(../cmake/copy_qt5_dlls_to_bin_dir.cmake)
 

openhantek/src/hantekdso/hantekdsocontrol.cpp
@@ -496,9 +496,9 @@ Dso::ErrorCode HantekDsoControl::setCoupling(ChannelID channel, Dso::Coupling co
     if (channel >= specification->channels)
         return Dso::ErrorCode::PARAMETER;
 
-    modifyCommand<ControlSetCoupling>(ControlCode::CONTROL_SETCOUPLING)
-            ->setCoupling(channel, coupling == Dso::Coupling::DC);
-
+    if (hasCommand(ControlCode::CONTROL_SETCOUPLING)) // don't send command if it is not implemented (like on the 6022)
+        modifyCommand<ControlSetCoupling>(ControlCode::CONTROL_SETCOUPLING)
+                ->setCoupling(channel, coupling == Dso::Coupling::DC);
     controlsettings.voltage[channel].coupling = coupling;
     return Dso::ErrorCode::NONE;
 }
@@ -734,6 +734,10 @@ Dso::ErrorCode HantekDsoControl::stringCommand(const QString &commandString) {
         return Dso::ErrorCode::UNSUPPORTED;
 }
 
+bool HantekDsoControl::hasCommand(Hantek::ControlCode code)
+{
+    return (control[uint8_t(code)] != 0);
+}
 
 void HantekDsoControl::addCommand(ControlCommand *newCommand, bool pending) {
     newCommand->pending = pending;

openhantek/src/hantekdso/hantekdsocontrol.h
@@ -100,6 +100,7 @@ class HantekDsoControl : public QObject {
     /// \return See ::Dso::ErrorCode.
     Dso::ErrorCode stringCommand(const QString &commandString);
 
+    bool hasCommand(Hantek::ControlCode code);
     void addCommand(ControlCommand *newCommand, bool pending = true);
     template <class T> T *modifyCommand(Hantek::ControlCode code) {
         control[(uint8_t)code]->pending = true;

commit 5664ff42d2585fb556a01bccd940f3624ad72a61

fix #33 - regression was introduced by 3f6b16c
Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

openhantek/src/OH_BUILD.h
@@ -1,2 +1,2 @@
 // Do not edit, will be re-created at each commit!
-#define OH_BUILD "20191109 build 543"
+#define OH_BUILD "20191109 build 544"

openhantek/src/docks/VoltageDock.cpp
@@ -159,6 +159,8 @@ void VoltageDock::setAttn(ChannelID channel, bool attn) {
     channelBlocks[channel].gainComboBox->addItems( attn ? attnStrings : gainStrings );
     channelBlocks[channel].gainComboBox->setCurrentIndex( index );
     channelBlocks[channel].attnCheckBox->setChecked(attn);
+    scope->voltage[channel].probeUsed = attn;
+    scope->voltage[channel].probeAttn = attn ? ATTENUATION : 1.0;
 }
 
 void VoltageDock::setMode(unsigned mathModeIndex) {