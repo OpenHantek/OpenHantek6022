310ae68629903d1f317f650a7beac875f1025cbd
Martin <Ho-Ro@users.noreply.github.com>
2020-12-11 16:41:18 +0100

    provide cat and inf file for Windows build
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- cmake/libusb_on_windows.cmake
+++ cmake/libusb_on_windows.cmake
@@ -8,11 +8,14 @@ endif()
 #    "${CMAKE_BINARY_DIR}/libusb.7z" )
 #endif()
 
-set(filename "${CMAKE_CURRENT_LIST_DIR}/libusb-1.0.21-win.7z")
+# set file names for lib and associated inf/cat files
+set(LIBUSB_7Z "${CMAKE_CURRENT_LIST_DIR}/libusb-1.0.21-win.7z")
+set(HANTEK_6022B_CAT "${CMAKE_CURRENT_LIST_DIR}/Hantek_6022B.cat")
+set(HANTEK_6022B_INF "${CMAKE_CURRENT_LIST_DIR}/Hantek_6022B.inf")
 set(LIBUSB_DIR "${CMAKE_BINARY_DIR}/libusb-1.0.21-win")
 
 execute_process(
-COMMAND ${CMAKE_COMMAND} -E tar xzf "${filename}"
+COMMAND ${CMAKE_COMMAND} -E tar xzf "${LIBUSB_7Z}"
 WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
 )
 
@@ -28,9 +31,12 @@ endif()
 target_link_libraries(${PROJECT_NAME} "${LIBUSB_DIR}/${ARCH}/libusb-1.0.lib")
 target_include_directories(${PROJECT_NAME} PRIVATE "${LIBUSB_DIR}" "${LIBUSB_DIR}/libusb-1.0")
 
+# execute commands
 add_custom_command(TARGET ${PROJECT_NAME}
         POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different "${LIBUSB_DIR}/${ARCH}/libusb-1.0.dll" $<TARGET_FILE_DIR:${PROJECT_NAME}>
-        COMMENT "Copy libusb-1 dlls for ${PROJECT_NAME}"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${HANTEK_6022B_CAT}" $<TARGET_FILE_DIR:${PROJECT_NAME}>
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${HANTEK_6022B_INF}" $<TARGET_FILE_DIR:${PROJECT_NAME}>
+        COMMENT "Copy libusb-1 dlls and inf/cat files for ${PROJECT_NAME}"
 )
 
