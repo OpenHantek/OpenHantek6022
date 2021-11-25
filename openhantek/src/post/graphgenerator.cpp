af2578e54cd395c43db0fe0c353df35fd7ed1715
Martin <Ho-Ro@users.noreply.github.com>
2021-11-25 12:57:52 +0100

    update user manual (regarding #246)
    The oscilloscope shall be connected to an HS-USB bus alone so that
    it works undisturbed and also does not interfere with other devices.
    
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- openhantek/src/post/graphgenerator.cpp
+++ openhantek/src/post/graphgenerator.cpp
@@ -112,7 +112,7 @@ void GraphGenerator::generateGraphsTYvoltage( PPresult *result ) {
 
         // sinc interpolation if there are too less samples on screen
         // https://ccrma.stanford.edu/~jos/resample/resample.pdf
-        if ( interpolationSinc && dotsOnScreen < view->screenWidth ) { // < 1 µs/div
+        if ( interpolationSinc && dotsOnScreen < view->screenWidth ) {
             // we would need sincWidth, but we take what we get
             const unsigned int left = std::min( sincWidth, unsigned( leftmostSample ) );
             horizontalFactor /= oversample;                                     // distance between (resampled) dots
