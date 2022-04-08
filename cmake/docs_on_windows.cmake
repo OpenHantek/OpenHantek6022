10771292c7465204f0c20817f041d228423ea393
Martin <Ho-Ro@users.noreply.github.com>
2022-04-08 11:47:28 +0200

    Update windows doc install
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- cmake/docs_on_windows.cmake
+++ cmake/docs_on_windows.cmake
@@ -10,9 +10,9 @@ set(DOCUMENTS "documents")
 add_custom_command(TARGET ${PROJECT_NAME}
         POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E make_directory ${DOCS}
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${DOCS_DIR}/OpenHantek6022_User_Manual.pdf" $<TARGET_FILE_DIR:"${PROJECT_NAME}/${DOCUMENTS}">
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${DOCS_DIR}/HANTEK6022_AC_Modification.pdf" $<TARGET_FILE_DIR:"${PROJECT_NAME}/${DOCUMENTS}">
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${DOCS_DIR}/HANTEK6022_Frequency_Generator_Modification.pdf" $<TARGET_FILE_DIR:"${PROJECT_NAME}/${DOCUMENTS}">
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${DOCS_DIR}/OpenHantek6022_zadig_Win10.pdf" $<TARGET_FILE_DIR:"${PROJECT_NAME}/${DOCUMENTS}">
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${DOCS_DIR}/OpenHantek6022_User_Manual.pdf" "${PROJECT_NAME}/${DOCUMENTS}"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${DOCS_DIR}/HANTEK6022_AC_Modification.pdf" "${PROJECT_NAME}/${DOCUMENTS}"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${DOCS_DIR}/HANTEK6022_Frequency_Generator_Modification.pdf" "${PROJECT_NAME}/${DOCUMENTS}"
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different "${DOCS_DIR}/OpenHantek6022_zadig_Win10.pdf" "${PROJECT_NAME}/${DOCUMENTS}"
         COMMENT "Copy manual and documentation files for ${PROJECT_NAME}"
 )
