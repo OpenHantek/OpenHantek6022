f986d63c62af89df2bfad27cbf8ade285dfc5c6a
Martin <Ho-Ro@users.noreply.github.com>
2023-12-31 14:05:43 +0100

    prepare next release 3.4
    Signed-off-by: Martin <Ho-Ro@users.noreply.github.com>

--- openhantek/src/documents.h
+++ openhantek/src/documents.h
@@ -5,18 +5,18 @@
 #include <QString>
 
 // manual and modification docs
-const QString UserManualName = QString( "OpenHantek6022_User_Manual.pdf" );
-const QString ACModificationName = QString( "HANTEK6022_AC_Modification.pdf" );
-const QString FrequencyGeneratorModificationName = QString( "HANTEK6022_Frequency_Generator_Modification.pdf" );
+const QString UserManualName( "OpenHantek6022_User_Manual.pdf" );
+const QString ACModificationName( "HANTEK6022_AC_Modification.pdf" );
+const QString FrequencyGeneratorModificationName( "HANTEK6022_Frequency_Generator_Modification.pdf" );
 
 // where are the (local) documents?
 #if defined( Q_OS_WIN )
-const QString DocPath = QString( "documents\\" );
+const QString DocPath( "documents\\" );
 #elif defined( Q_OS_FREEBSD )
-const QString DocPath = QString( "/usr/local/share/doc/openhantek/" );
+const QString DocPath( "/usr/local/share/doc/openhantek/" );
 #else
-const QString DocPath = QString( "/usr/share/doc/openhantek/" );
+const QString DocPath( "/usr/share/doc/openhantek/" );
 #endif
 
 // GitHub doc location
-const QString DocUrl = QString( "https://github.com/OpenHantek/OpenHantek6022/blob/main/docs/" );
+const QString DocUrl( "https://github.com/OpenHantek/OpenHantek6022/blob/main/docs/" );
